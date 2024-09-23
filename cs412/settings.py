483c9a98bf17b763964bcc4b02eab16d27cf1995
Rigpea <rigpea@bu.edu>
2024-09-23 00:56:33 -0400

    created static files holer

--- cs412/settings.py
+++ cs412/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [
 ]
 
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
