28844e98743763d4fa41b17dfe5defaed8c4bae4
Rigpea <rigpea@bu.edu>
2024-09-30 19:38:04 -0400

    css fixes

--- cs412/urls.py
+++ cs412/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('quotes/', include('quotes.urls')),  # Map root URL to the quotes app
+    path('', include('quotes.urls')),  # Map root URL to the quotes app
     path('restaurant/', include('restaurant.urls') )
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
